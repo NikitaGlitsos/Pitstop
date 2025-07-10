06e49cdd275c4292390ecf5bd357284eb8e6c937
Никита Глицос <glitsosnikita84@gmail.com>
2025-07-10 15:58:15 +0300

    gotovo4

--- gg.js/index.js
+++ gg.js/index.js
@@ -16,24 +16,21 @@ window.addEventListener('scroll', function () {
 window.addEventListener('scroll', checkBlocks);
 
 const elements = document.querySelectorAll('.stok-img-container'); // Замените на свой селектор
+const elements1 = document.querySelectorAll('.stok-img-container');
 
 elements.forEach(element => {
     element.addEventListener('touchstart', function(event) {
         event.preventDefault(); // Предотвращает стандартное поведение (например, выделение текста)
         const imgList = this.querySelector('.stok-img-list')
         if (imgList) {
-            if (imgList.classList.contains('active')) {
-                imgList.classList.remove('active');
-            } else {
-                imgList.classList.add('active');
-            }
+            imgList.classList.add('active');            
         }
 });
 });
 
 document.addEventListener('touchstart', function(event) {
     // Проверяем, был ли клик внутри элемента .stok-img-container
-    if (!event.target.closest('.stok-img-container')) {
+    if (!event.target.closest('.stok-img-container') && event.target.closest('.stok-img-container')) {
       // Если клик был снаружи, закрываем все imgList
         const allImgLists = document.querySelectorAll('.stok-img-list.active');
         allImgLists.forEach(imgList => {
