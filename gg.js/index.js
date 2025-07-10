c4a607d1f81bae0af32ba8294c61411bb39d1a51
Никита Глицос <glitsosnikita84@gmail.com>
2025-07-10 16:10:58 +0300

    ggg

--- gg.js/index.js
+++ gg.js/index.js
@@ -23,15 +23,19 @@ elements.forEach(element => {
         event.preventDefault(); // Предотвращает стандартное поведение (например, выделение текста)
         const imgList = this.querySelector('.stok-img-list')
         if (imgList) {
-            imgList.classList.add('active');            
+            if (event.target.closest('.stok-img-container')) {
+                imgList.classList.remove('active');
+            } else  {
+                imgList.classList.add('active');
+            }
         }
 });
 });
 
 document.addEventListener('touchstart', function(event) {
     // Проверяем, был ли клик внутри элемента .stok-img-container
-    if (!event.target.closest('.stok-img-container') || event.target.closest('.stok-img-container')) {
-
+    if (!event.target.closest('.stok-img-container')) {
+      // Если клик был снаружи, закрываем все imgList
         const allImgLists = document.querySelectorAll('.stok-img-list.active');
         allImgLists.forEach(imgList => {
             imgList.classList.remove('active');
