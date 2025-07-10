3c78cb8d9b13c6e4621ebb31b48093f611ea48b0
Никита Глицос <glitsosnikita84@gmail.com>
2025-07-10 15:11:50 +0300

    gotovo2

--- gg.js/index.js
+++ gg.js/index.js
@@ -14,3 +14,19 @@ window.addEventListener('scroll', function () {
 
 // Вызываем функцию при загрузке страницы и при прокрутке
 window.addEventListener('scroll', checkBlocks);
+
+const elements = document.querySelectorAll('.stok-img-container'); // Замените на свой селектор
+
+elements.forEach(element => {
+    element.addEventListener('touchstart', function(event) {
+        event.preventDefault(); // Предотвращает стандартное поведение (например, выделение текста)
+        const imgList = this.querySelector('.stok-img-list')
+        if (imgList) {
+            if (imgList.classList.contains('active')) {
+                imgList.classList.remove('active');
+            } else {
+                imgList.classList.add('active');
+            }
+        }
+});
+});
